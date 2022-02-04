35d07f96881a15a5dfb88e5aadb8d72de1b92ac8
Lee Roman <58044796+Velmor99@users.noreply.github.com>
2022-02-04 22:22:42 +0200

    done edit functional and add edit page (#3)

--- routes/courses.js
+++ routes/courses.js
@@ -1,14 +1,38 @@
-const {Router} = require('express')
-const router = Router()
-const coursesModel = require('../models/courses')
+const { Router } = require("express");
+const router = Router();
+const coursesModel = require("../models/courses");
 
-router.get('/', async (req, res) => {
-    const courses = await coursesModel.getAll()
-    res.render('courses', {
-        isCourses: true,
-        title: "Курсы",
-        courses: courses
-    })
+router.get("/", async (req, res) => {
+  const courses = await coursesModel.getAll();
+  res.render("courses", {
+    isCourses: true,
+    title: "Курсы",
+    courses: courses,
+  });
+});
+
+router.get("/:id", async (req, res) => {
+  const course = await coursesModel.findById(req.params.id);
+  res.render("course", {
+    layout: "empty",
+    course: course,
+  });
+});
+
+router.get("/:id/edit", async (req, res) => {
+  if (!req.query.allow) {
+    res.redirect("/");
+  }
+  const course = await coursesModel.findById(req.params.id);
+  res.render("courses-edit", {
+    title: `Редактировать курс ${course.title}`,
+    course: course,
+  });
+});
+
+router.post("/edit", async (req, res) => {
+    await coursesModel.changeCourse(req.body)
+    res.redirect('/courses')
 })
 
-module.exports = router
+module.exports = router;
