37ef1070dd2c2c09b49230d3d9af29b9d57f8a0a
Lee Roman <58044796+Velmor99@users.noreply.github.com>
2022-02-05 13:11:14 +0200

    done card markup and remove functional (#5)

--- public/script.js
+++ public/script.js
@@ -1,6 +1,48 @@
-document.querySelectorAll('.price').forEach(node => {
-    node.textContent = new Intl.NumberFormat('de-DE', {
-        currency: "eur",
-        style: 'currency'
-    }).format(node.textContent)
-})
+const toCurrency = (price) => {
+  return new Intl.NumberFormat("de-DE", {
+    currency: "eur",
+    style: "currency",
+  }).format(price);
+};
+
+document.querySelectorAll(".price").forEach((node) => {
+  node.textContent = toCurrency(node.textContent);
+});
+
+const $cart = document.querySelector("#cart");
+if ($cart) {
+  $cart.addEventListener("click", (e) => {
+    if (e.target.classList.contains("js-remove")) {
+      const id = e.target.dataset.id;
+      fetch("/cart/remove/" + id, {
+        method: "delete",
+      })
+        .then((res) => res.json())
+        .then((cart) => {
+          if (cart.courses.length) {
+            const html = cart.courses
+              .map((item) => {
+                return `
+                  <tr>
+                    <td>
+                        ${item.title}
+                    </td>
+                    <td>
+                        ${item.count}
+                    </td>
+                    <td>
+                        <button class="waves-effect waves-light btn-small js-remove" data-id="${item.id}">Удалить</button>
+                    </td>
+                </tr>
+                  `;
+              })
+              .join("");
+            $cart.querySelector("tbody").innerHTML = html;
+            $cart.querySelector(".price").textContent = toCurrency(cart.price);
+          } else {
+            $cart.innerHTML = `<p>Корзина пуста</p>`;
+          }
+        });
+    }
+  });
+}
--- routes/cart.js
+++ routes/cart.js
@@ -3,9 +3,12 @@ const router = Router();
 const coursesModel = require("../models/courses");
 const cartModel = require("../models/cart");
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
+  const data = await cartModel.getCart();
   res.render("cart", {
     isCart: true,
+    courses: data.courses,
+    price: data.price,
   });
 });
 
@@ -13,6 +16,11 @@ router.post("/add", async (req, res) => {
   const course = await coursesModel.findById(req.body.id);
   await cartModel.add(course);
   res.redirect("/cart");
+});
+
+router.delete("/remove/:id", async (req, res) => {
+  const cart = await cartModel.remove(req.params.id);
+  res.status(200).json(cart);
 });
 
 module.exports = router;
